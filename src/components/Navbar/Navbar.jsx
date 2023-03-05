75e1f53b38c8c70b559aac3a366813864291f8d4
vaidyanath b <b.vaidyanath003@gmail.com>
2023-03-05 19:51:39 +0530

    hmm

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+      
+          <div className="flex w-screen md:items-center border-b border-slate-800 py-3 px-6 align-middle">
+              <img src={logo} alt="GDC"/>
+              <nav className = "flex flex-1 justify-between text-[#FFFDD4] text-[20px] mx-auto">
+                    
+                      <span className = "ml-60 flex text-center">Home</span>
+                      <div className="justify-between flex mr-[40rem] px-4">
+                      <span className = "mx-8">Events</span>
+                      <span className = "ml-8">About us</span>
+                      </div>
+                  
+              </nav>
+
+
+          </div>
   )
 }
 
