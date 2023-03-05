37fd0d7c4c679a08d2d6e49f6b83b6faaa7d99ec
jzf21 <jozefneelamkavil2003@gmail.com>
2023-03-05 14:27:37 +0530

    tried

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Landing from "./components/Landing/Landing";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="flex h-100 flex-col px-5  ">
+      <Navbar/>
       <Landing />
+      <About/>
     </div>
   );
 }
