4c697e73a6b059c2bfdcc00d72c1f42708de7217
Amil P A <amilpa2020@gmail.com>
2023-03-14 21:30:38 +0530

    minor changes to h1

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,15 +3,15 @@ import "../designs.css";
 import "./Landing.css";
 
 const Landing = () => {
-  return (
-    <div className="relative overflow-x-hidden h-[100vh]">
-      <div className="design-box p-4"></div>
-      <h1 className="text-title w-full md:w-[50%] text-[4rem] md:text-[7rem] leading-[5rem] md:text-left text-center md:leading-[9rem] mt-[10rem] md:ml-5 ">
-        Game Development Club<span className="text-[3rem] md:text-[4rem]">MEC</span>
-      </h1>
-      <span></span>
-    </div>
-  );
+	return (
+		<div className="relative overflow-x-hidden h-[100vh]">
+			<div className="design-box p-4"></div>
+			<h1 className="text-title w-full md:w-[500px] text-[4rem] md:text-[7rem] leading-[5rem] md:text-left text-center md:leading-[9rem] mt-[10rem] md:ml-5 ">
+				Game Development Club<span className="text-[3rem] md:text-[4rem]">MEC</span>
+			</h1>
+			<span></span>
+		</div>
+	);
 };
 
 export default Landing;
