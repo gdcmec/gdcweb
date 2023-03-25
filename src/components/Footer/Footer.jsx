98a1833a3fbc608612f7b693daa2caccf8d10934
Amil P A <amilpa2020@gmail.com>
2023-03-25 15:04:29 +0530

    styling change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { FaFacebookSquare, FaLinkedin, FaTwitter , FaInstagram } from 'react-icons/fa'
+import { FaFacebookSquare, FaLinkedin, FaTwitter , FaInstagram , FaGoogle } from 'react-icons/fa'
 
 import logo from '../../assets/logo.svg';
 
@@ -11,11 +11,12 @@ const Footer = () => {
 				<p className='text-base'>Get in touch</p>
 				<p className='text-xl'>gdcmec@mec.ac.in</p>
 			</div>
-			<div className='flex'>
+			<div className='flex pr-8'>
 				<FaFacebookSquare size={ 25 } style={{marginRight:"2rem"}}/>
 				<FaLinkedin size={ 25 } style={{marginRight:"2rem"}}/>
 				<FaTwitter size={ 25 } style={{marginRight:"1.8rem"}}/>
 				<FaInstagram size={ 25 } style={{marginRight:"2rem"}}/>
+				<FaGoogle size={ 23 } style={{marginRight:"2rem"}}/>
 			</div>
 			<div className='mr-4 flex flex-col items-center gap-1'>
 				<img src={ logo } alt="true" className='w-[40px]'/>
