93ea4c58ce59c0231ada46a4bc341e1262b73cbf
jzf21 <jozefneelamkavil2003@gmail.com>
2023-04-07 11:28:53 +0530

    hmm cool

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -42,7 +42,18 @@ const Events = () => {
 
   return (
     <>
-      {popup && <div className="fixed top-0 bottom-0 h-screen w-screen z-10 bg-black opacity-70"></div>}
+      {popup && (
+        <motion.div
+          initial={{ height: 0 }}
+          animate={{ height: '100%' }}
+          transition={{
+            ease: 'linear',
+            duration: 0.5,
+            x: { duration: 0.5 },
+          }}
+          className="fixed top-0 bottom-0 h-screen w-screen z-10 bg-black opacity-70"
+        ></motion.div>
+      )}
       {popup && (
         <motion.div
           initial={{ opacity: 0 }}
@@ -50,6 +61,7 @@ const Events = () => {
           transition={{
             ease: 'linear',
             duration: 1,
+            delay:1,
             x: { duration: 1 },
           }}
           className="text-white transition ease-in-out delay-150 bg-black min-h-[50%] min-w-[50%] fixed z-10 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] p-10 border-2 border-[#FFF3D4] flex flex-col justify-center items-center gap-10"
