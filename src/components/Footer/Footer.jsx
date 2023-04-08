ae2e4e067f9283d739e130a21c8fc095f7f67396
Amil P A <amilpa2020@gmail.com>
2023-04-08 16:48:26 +0530

    fixed overflow issues and navbar responsivness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,18 +6,18 @@ import logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div className="text-white flex justify-between items-center h-[90px] border-t-[1px]">
+    <div className="text-white flex flex-col justify-center gap-5 md:justify-between items-start pt-4 h-[200px] border-t-[1px] md:flex-row md:h-[90px] md:items-center">
       <div className="ml-4">
         <p className="text-base">Get in touch</p>
         <p className="text-xl">gdcmec@mec.ac.in</p>
       </div>
-      <div className="flex">
+      <div className="flex ml-5">
         <FaFacebookSquare size={25} style={{ marginRight: '2rem' }} />
         <FaLinkedin size={25} style={{ marginRight: '2rem' }} />
         <FaTwitter size={25} style={{ marginRight: '1.8rem' }} />
         <FaInstagram size={25} style={{ marginRight: '2rem' }} />
       </div>
-      <div className="mr-4 flex flex-col items-center gap-1">
+      <div className="ml-3 md:mr-4 flex flex-row items-center gap-1 md:flex-col">
         <img src={logo} alt="true" className="w-[40px]" />
         <p className="text-sm">GDC CLUB,MEC</p>
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import './Navbar.css';
 import { useState,useEffect } from 'react';
 
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { RxCross1 } from 'react-icons/rx'
 
 const Navbar = () => {
 
 	const [scrollbar, setscrollbar] = useState(false)
+	const [mobilenav, setmobilenav] = useState(false)
 
 	const setGlassyEffect = () => {
 		if (window.scrollY>50) {
@@ -17,18 +19,21 @@ const Navbar = () => {
 			setscrollbar(false)
 		}
 	}
-
 	window.addEventListener('scroll' , setGlassyEffect)
 
+	const handleHamburger = () => {
+		setmobilenav(!mobilenav)
+	}
+
 	return (
-		<div className={ `w-full py-3 fixed top-0 z-[2] flex items-center justify-between pl-2 ${scrollbar ? 'bg-black' : ''}`}>
+		<div className={ `w-screen flex items-center justify-between py-3 fixed top-0 z-[2] pl-2 ${scrollbar ? 'bg-black' : ''}`}>
 			<img src={logo} alt="true" className='h-[50px]'/>
-			<button className='text-white static md:hidden'><GiHamburgerMenu/></button>
-			<ul className='text-white hidden md:block'>
-				<li className='inline ml-8'>Home</li>
-				<li className='inline ml-8'>About</li>
-				<li className='inline ml-8'>Events</li>
-				<li className='inline ml-8 mr-5'>Contact</li>
+			<button onClick={handleHamburger} className='text-white text-3xl block md:hidden mr-3'>{ mobilenav ? <RxCross1/> : <GiHamburgerMenu/>}</button>
+			<ul className={ `text-white md:static md:w-max md:h-max md:pt-0 md:bg-inherit md:block md:mr-3 fixed top-0 w-screen pt-24 h-screen z-[-1] text-center bg-black ${mobilenav ? 'right-0':'right-full'}` }>
+				<li className='block m-auto mb-3 md:inline md:ml-8'>Home</li>
+				<li className='block m-auto mb-3 md:inline md:ml-8'>About</li>
+				<li className='block m-auto mb-3 md:inline md:ml-8'>Events</li>
+				<li className='block m-auto mb-3 md:inline md:ml-8'>Contact</li>
 			</ul>
 		</div>
 	);
