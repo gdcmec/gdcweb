65f7041c96e5a75df0e9417873b860412e6016ca
jzf21 <jozefneelamkavil2003@gmail.com>
2023-04-08 17:49:55 +0530

    made changes in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import './About.css';
 import '../designs.css';
 const About = () => {
-  const [id,setId] =useState(0);
+  const [id, setId] = useState(0);
   return (
     <div className="relative h-auto md:h-[110vh] py-10 gap-8 overflow-hidden gradient">
       {/* <div className="design-box z-[-1]"></div> */}
-      <h2 class="text-3xl md:text-8xl text-[#FFF3D4] text-right w-1/3 ml-auto mr-[3.5rem]">About us</h2>
+      <h2 class="text-3xl md:text-6xl text-[#FFF3D4] text-center self-center md:mb-10">About us</h2>
       <div className=" p-4 "></div>
-      <div className="grid grid-cols-1 md:grid-cols-2 px-[6.8rem]">
+      <div className="grid grid-cols-1 md:grid-cols-2 px-[6.8rem] py-5">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXQ9F0hvfW0twxKZG7E5Oq3o1KduSrVXMMrw&usqp=CAU"
           className="flex-end"
         />
         {id == 0 && (
-          <div className="flex flex-col justify-center gap-2 px-4">
+          <div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{duration:1}}
+            className="flex flex-col justify-center gap-2 px-4"
+          >
             <h2 className="text-[#FFBF9B] uppercase text-[0.7rem]">01. What we do</h2>
             <h2 className="text-white text-2xl text-left">Pushing Our Limits With Game Development</h2>
             <p className="text-[#afafc0] text-[1rem]">
@@ -22,7 +28,7 @@ const About = () => {
               to learn about various aspects of game development, such as programming, design, art, sound, and project
               management.
             </p>
-            <div className="flex flex-row mt-5 space-between justify-center items-center">
+            <div className="flex flex-row mt-5 space-between justify-center gap-2 items-center">
               <button onClick={() => setId(0)} className="w-10 h-10 text-center align-center rounded-full bg-white">
                 1
               </button>
@@ -46,7 +52,7 @@ const About = () => {
               to learn about various aspects of game development, such as programming, design, art, sound, and project
               management.
             </p>
-            <div className="flex flex-row mt-5 space-between justify-center items-center">
+            <div className="flex flex-row mt-5 space-between justify-center items-center gap-2">
               <button onClick={() => setId(0)} className="w-10 h-10 text-center align-center rounded-full bg-white">
                 1
               </button>
@@ -70,7 +76,7 @@ const About = () => {
               to learn about various aspects of game development, such as programming, design, art, sound, and project
               management.
             </p>
-            <div className="flex flex-row mt-5 space-between justify-center items-center">
+            <div className="flex flex-row mt-5 space-between justify-center items-center gap-2">
               <button onClick={() => setId(0)} className="w-10 h-10 text-center align-center rounded-full bg-white">
                 1
               </button>
@@ -86,21 +92,11 @@ const About = () => {
           </div>
         )}
       </div>
-      {/* <div className="grid  grid-cols-1 md:grid-cols-3 gap-8 md:gap-0 about ">
-        <div>
-          <Aboutcontent child="What We Do" />
-        </div>
-        <div>
-          <Aboutcontent child="Why We do it " />
-        </div>
-        <div>
-          <Aboutcontent child={"How we do it"} />
-        </div>
-      </div> */}
+  
     </div>
   );
 };
-const Aboutcontent = ({child}) => {
+const Aboutcontent = ({ child }) => {
   return (
     <div className="flex flex-col w-full min-h-[300px] px-4 columnDivider justify-start gap-8">
       <h1 className="text-white text-3xl underline">{child}</h1>
---
+++ src/components/Navold/Navold.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Navbar.css';
+import { useState, useEffect } from 'react';
+
+const Navbar = () => {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPos(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const navbarClasses = scrollPos > 50 ? 'nav bg-black' : 'nav';
+  return (
+    <>
+      <div className={`hidden md:flex fixed w-screen items-center   py-2 px-6 align-middle z-[2] ${navbarClasses}`}>
+        <img src={logo} width="70px" height="70px" alt="GDC" />
+        <nav className="hidden  md:flex gap-16 flex-1 md:justify-end text-white text-[20px] mr-[5%]">
+          <a class="group text-white transition-all duration-300 ease-in-out" href="#">
+            <span class="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+              Home
+            </span>
+          </a>
+          <a class="group text-white transition-all duration-300 ease-in-out" href="#">
+            <span class="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+              About
+            </span>
+          </a>
+          <a class="group text-white transition-all duration-300 ease-in-out" href="#">
+            <span class="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+              Events
+            </span>
+          </a>
+          <a class="group text-white transition-all duration-300 ease-in-out" href="#">
+            <span class="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+              Contact
+            </span>
+          </a>
+        </nav>
+      </div>
+      <nav className=" md:hidden block  ">
+        <div class="sticky z-40 navbar ">
+          <div class="container nav-container fixed">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines z-50 relative">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>
+            <div class="logo">
+              <img src={logo} width="60px" height="60px" alt="GDC" />
+            </div>
+            <div class=" menu-items">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Events</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
