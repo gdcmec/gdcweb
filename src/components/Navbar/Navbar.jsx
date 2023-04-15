72ed851116d4af18444f3d1c662129ff04556ea8
amilpa <amilpa2020@gmail.com>
2023-04-15 20:02:10 +0530

    changed bg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,15 +10,26 @@ const Navbar = () => {
   const [scrollbar, setscrollbar] = useState(false);
   const [mobilenav, setmobilenav] = useState(false);
 
+	const handleScroll = () => {
+		if (window.scrollY>50) {
+			setscrollbar(true)	
+		}
+		else{
+			setscrollbar(false)
+		}
+	}
+
+	window.addEventListener('scroll', handleScroll)
+
 	const handleHamburger = () => {
 		setmobilenav(!mobilenav)
 	}
 
 	return (
-		<div className='w-screen h-[90px] bg-[#0c0c0c] flex items-center justify-between py-3 fixed top-0 z-[2] px-5'>
-			<img src={logo} alt="true" className='h-[50px]'/>
+		<div className={`w-screen h-[70px] flex items-center justify-between py-3 fixed top-0 z-[2] px-5 ${scrollbar ? 'bg-[#0c0502]':'bg-none'}`}>
+			<img src={logo} alt="true" className='h-[40px]'/>
 			<button onClick={handleHamburger} className='text-white text-3xl block md:hidden mr-3'>{ mobilenav ? <RxCross1/> : <GiHamburgerMenu/>}</button>
-			<ul className={ `text-white font-medium md:static md:w-max md:h-max md:pt-0 md:bg-inherit md:block md:mr-3 fixed top-0 w-screen pt-24 h-screen z-[-1] text-center bg-black ${mobilenav ? 'right-0':'right-full'}` }>
+			<ul className={ `text-white font-medium md:static md:w-max md:h-max md:pt-0 md:bg-transparent md:block md:mr-3 fixed top-0 w-screen pt-24 h-screen z-[-1] text-center bg-black ${mobilenav ? 'right-0':'right-full'}` }>
 				<li className='block m-auto mb-3 md:inline md:ml-8'><a href='#home'>Home</a></li>
 				<li className='block m-auto mb-3 md:inline md:ml-8'><a href='#about'>About</a></li>
 				<li className='block m-auto mb-3 md:inline md:ml-8'><a href='#events'>Events</a></li>
