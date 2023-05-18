d0cb534309612d4477516d1cfc536869c011a755
jzf21 <jozefneelamkavil2003@gmail.com>
2023-05-18 13:34:21 +0530

    center

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -3,12 +3,12 @@ import ValoCard from '../ValoCard/ValoCard';
 
 const CardContainer = () => {
   return (
-    <div className='grid grid-cols-3  py-5 w-full items-center '>
-        <ValoCard/>
-        <ValoCard/>
-        <ValoCard/>
+    <div className="grid grid-cols-1 md:grid-cols-3 content-center items-center px-5 py-5 w-full">
+      <ValoCard />
+      <ValoCard />
+      <ValoCard />
     </div>
-  )
+  );
 }
 
 export default CardContainer
