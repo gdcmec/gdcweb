1b898edf511398bdbc60490cb8f8c577aef6324f
jzf21 <jozefneelamkavil2003@gmail.com>
2023-05-28 14:14:00 +0530

    push

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -7,7 +7,7 @@ const CardContainer = () => {
       <div className="">
         <h1 className="text-3xl text-center font-semibold text-white">Events</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 content-center items-center px-5 py-5 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-3 content-center items-center gap-8 px-5 py-5 w-full">
         <ValoCard title={'Unity Workshop'} venue={'CCF '} date={'12/07/22'} />
         <ValoCard title={'Unity Workshop'} venue={'CCF '} date={'12/07/22'} />
         <ValoCard title={'Unity Workshop'} venue={'CCF '} date={'12/07/22'} />
