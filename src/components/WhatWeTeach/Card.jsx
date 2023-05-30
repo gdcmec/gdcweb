0e80559d243645499686235eb90c9437df06b998
amilpa <amilpa2020@gmail.com>
2023-05-30 23:05:06 +0530

    changes whatweteach

--- src/components/WhatWeTeach/Card.jsx
+++ src/components/WhatWeTeach/Card.jsx
@@ -1,19 +1,18 @@
+import React from 'react';
 
-import React from 'react'
+const Card = ({ index, item }) => {
+  return (
+    <section key={index} className="relative py-[50px] h-max">
+      <div className="timeline-parent w-max h-max">
+        <div className="timeline-content py-[35px] h-max">
+          <div className="flex flex-col items-center h-max gap-3">
+            <h2 className="text-primary text-xl font-semibold">{item.title}</h2>
+            <p className="w-[300px] text-center text-gray-300">{item.description}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-const Card = ({index,item}) => {
-	return (
-		<section key ={index} className='relative py-[50px] h-max'>
-			<div className='timeline-parent w-max h-max'>
-				<div className='timeline-content py-[35px] h-max'>
-					<div className='flex flex-col items-center h-max gap-3'>
-						<h2 className='text-[#fff3d5] text-xl'>{item.title}</h2>
-						<p className='w-[300px] text-center'>{item.description}</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
-}
-
-export default Card
+export default Card;
