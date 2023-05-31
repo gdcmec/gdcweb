cc5e30b7cd4af8ac1cad543c47d188db0627f421
jzf21 <jozefneelamkavil2003@gmail.com>
2023-05-31 15:33:50 +0530

    added register button

--- src/components/ValoCard/ValoCard.jsx
+++ src/components/ValoCard/ValoCard.jsx
@@ -23,13 +23,18 @@ const ValoCard = ({ event }) => {
           </p>
         </div>
         <div class="card-footer">
-          <p className='font-black text-xl tracking-wide text-black'>{date}</p>
+          {/* <p className="font-black text-xl tracking-wide text-black">{date}</p> */}
           <p>
             We host a variety of events, including seminars, game jams, competitions, and guest lectures to promote
             learning and creativity.The club also provides chances to promote and publish games, connect with other game
             makers, and learn useful industry insights
           </p>
-          <button>Register</button>
+          <button class="btn">
+            <span class="btn__inner">
+              <span class="btn__slide"></span>
+              <span class="btn__content">REGISTER</span>
+            </span>
+          </button>
         </div>
         <div class="background-image">
           <img src="https://rare-gallery.com/thumbnail/394521-valorant-game-agents-hero-4k-pc-wallpaper.jpg" />
